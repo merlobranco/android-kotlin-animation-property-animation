fdc2efe9964659e9c4d665d0de69638920ea92c5
Brais Cidras <braisomary@gmail.com>
2020-04-03 13:36:51 +0200

    Adding Translate Animation

--- app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -89,6 +89,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun translater() {
+        val animator = ObjectAnimator.ofFloat(star, View.TRANSLATION_X, 200f)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.start()
     }
 
     private fun scaler() {
